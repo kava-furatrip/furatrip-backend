9a3ccc570309e27fcbcde10bc15512044526af31
kava-furatrip <yukava25@gmail.com>
2025-05-23 13:34:18 +0300

    Fix: порт и bind address для Render

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const driverRoutes = require('./routes/driverRoutes');
 const tripRoutes = require('./routes/tripRoutes'); // ⬅️ добавили
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 100000;
 
 app.use(cors());
 app.use(express.json());
